b08d57242f087c65fd215619633a52e81b4de415
Sunhak Lov <sunhaklov04@gmail.com>
2025-09-30 16:05:09 -0700

    add some take for backend api

--- Backend/server.js
+++ Backend/server.js
@@ -9,3 +9,25 @@ app.get("/", (req, res) => {
 app.listen(8000, () => {
     console.log('server is running')
 })
+
+
+/*
+========== TO-DO ==========
+=> Create Users (applicants / employee) "POST GET UPDATE DELETE"
+ 
+=> Create Application (applicants / employee) "POST GET UPDATE DELETE"
+ 
+=> Create JobListing (applicants / employee) "POST GET UPDATE DELETE"
+ 
+=> API 'GET' all job
+ 
+=> API 'GET' search & filter
+ 
+=> API 'GET' all job applied with status
+ 
+=> API 'GET' all job posted
+ 
+=> API 'GET' all applicant applied for the job
+ 
+*/
+
