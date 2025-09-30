fbb79db6d5456144e1abe53d744bd92c80a78095
Sunhak Lov <sunhaklov04@gmail.com>
2025-09-30 14:52:16 -0700

    setup backend

--- Backend/configs/db.js
+++ Backend/configs/db.js
@@ -1,10 +1,23 @@
 import mysql from "mysql2";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+console.log("ENV CHECK:", {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD ? "****" : undefined,
+    database: process.env.DB_NAME
+});
 
 // Use Pool + Promises
 const pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "085583436Hak$$$$",
-    database: "jobBoard",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 }).promise();
 
+const [data] = await pool.query("SELECT * FROM tests");
+
+console.log(data)
