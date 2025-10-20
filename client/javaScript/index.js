804ea89e1e04057b8213706147b34ddc6c09a516
Sunhak Lov <sunhaklov04@gmail.com>
2025-10-20 15:40:19 -0700

    update functionality of the candidateSettingBtn in employer-candiate.html

--- client/javaScript/index.js
+++ client/javaScript/index.js
@@ -2,7 +2,11 @@
 const jobListingContainer = document.querySelector(".job-list-container")
 const jobDetailPopupContainer = document.querySelector(".job-detail-container-popup")
 const closejobPopupContainerBtn = document.querySelector(".close-popup")
-
+const settingCandidateBtns = document.querySelectorAll(".settingCandidateBtn")
+const settingCandidateScreen = document.querySelector(".settingCandidateScreen")
+const closeSidebar = document.querySelector(".closeSidebar")
+const mainContent = document.querySelector(".main-content");
+const candidateList = document.querySelector("ul[role='list']");
 
 function popJobDetail() {
     jobListingContainer.classList.add("hidden");
@@ -13,5 +17,34 @@ closejobPopupContainerBtn.addEventListener("click", () => {
     jobListingContainer.classList.remove("hidden");
     jobDetailPopupContainer.classList.add("hidden");
 })
+
+settingCandidateBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        const li = btn.closest("li");
+        const { id, name, email } = li.dataset;
+        settingCandidateScreen.classList.remove("w-[0]");
+        settingCandidateScreen.classList.add("w-[40vw]");
+        mainContent.classList.add("opacity-50");
+        settingCandidateScreen.innerHTML = "";
+        settingCandidateScreen.insertAdjacentHTML("beforeend", `
+            <button class="closeSidebar text-white p-2 font-bold bg-black">Close</button>
+            <p>hi user with ${id} & named "${name}" & email is ${email}</p>
+        `);
+        const closeSidebar = settingCandidateScreen.querySelector(".closeSidebar");
+        closeSidebar.addEventListener("click", () => {
+            const screen = closeSidebar.parentElement;
+            screen.classList.remove("w-[40vw]");
+            screen.classList.add("w-[0]");
+            mainContent.classList.remove("opacity-50");
+        });
+    })
+})
+
+// closeSidebar.addEventListener("click", () => {
+//     const screen = closeSidebar.parentElement;
+//     screen.classList.remove("w-[40vw]");
+//     screen.classList.add("w-[0]");
+//     mainContent.classList.remove("opacity-50");
+// })
 
 // Working on Fecthing and Updating UI
